f46fe97799dce959559d6d25253da39ff8cdc821
almiheenko <al.miheenko@gmail.com>
2021-09-16 11:42:51 +0300

    make and setup fixes

--- setup.py
+++ setup.py
@@ -13,21 +13,13 @@ from setuptools import setup
 from distutils.command.build import build as DistutilsBuild
 from distutils.spawn import find_executable
 
-from tandemmapper.__version__ import __version__
+from tandemmapper2.__version__ import __version__
 
 
 # Make sure we're running from the setup.py directory.
 script_dir = os.path.dirname(os.path.realpath(__file__))
 if script_dir != os.getcwd():
     os.chdir(script_dir)
-
-
-requirements_fn = os.path.join(script_dir, 'requirements.txt')
-requirements = []
-with open(requirements_fn) as f:
-    for line in f:
-        line = line.strip()
-        requirements.append(line)
 
 
 description = \
@@ -39,29 +31,32 @@ genome assemblies (including long repetitive regions)
 
 class MakeBuild(DistutilsBuild):
     def run(self):
+        os.chdir(os.path.join(script_dir, "tandemmapper2"))
         if not find_executable("make"):
             sys.exit("ERROR: 'make' command is unavailable")
         try:
             subprocess.check_call(["make"])
         except subprocess.CalledProcessError as e:
             sys.exit("Compilation error: ", e)
+        os.chdir(script_dir)
         DistutilsBuild.run(self)
 
 
 setup(
     name="TandemMapper2",
-    version=__version__,
+    version="2.0.0",
     description=description,
     url='https://github.com/ablab/TandemMapper2',
     author='Alla Mikheenko',
     author_email='al.miheenko@gmail.com',
     license='GNU General Public License v3.0',
-    install_requires=requirements,
+    install_requires=[
+    'plotly', 'python-slugify', 'biopython', 'numpy'],
     packages=['tandemmapper2'],
     package_dir={'tandemmapper2': 'tandemmapper2'},
     package_data={'tandemmapper2': ['build/bin/tandem_mapper', 'config/*', '*', 'py_src/*', 'test_dataset/*',]},
     entry_points={
-        'console_scripts': ['tandemmapper2=tandemmapper2.tandemmapper2:main']
+        'console_scripts': ['tandemmapper2=tandemmapper2.main:main']
     },
     cmdclass={'build': MakeBuild}
 )
