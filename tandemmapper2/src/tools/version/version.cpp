b1a47eb830e09f88c4d1c7d1251be740d9fce05e
seryrzu <seryrzu@gmail.com>
2021-11-07 14:20:29 -0800

    version.cpp should not be in the repo

--- tandemmapper2/src/tools/version/version.cpp
+++
@@ -1,7 +0,0 @@
-#include "version.hpp"
-
-using namespace tools;
-
-const std::string Version::GIT_SHA1 = "f0dcada840282dff8600f9073f5cfed9f7ada237-dirty";
-const std::string Version::GIT_DATE = "Fri Oct 15 16:38:47 2021";
-const std::string Version::GIT_COMMIT_SUBJECT = "Phony commit to provoke TeamCity";
