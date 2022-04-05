846f24d1b379d7a2c9a76b329c760134db65fcad
seryrzu <seryrzu@gmail.com>
2022-04-04 18:18:05 -0700

    Blank lines are now allowed in fasta files thanks to @AntonBankevich https://github.com/AntonBankevich/LJA/commit/b814d4284f5fb9fc9818a0a375845a9a9e560a20

--- veritymap/src/tools/sequences/seqio.hpp
+++ veritymap/src/tools/sequences/seqio.hpp
@@ -93,21 +93,15 @@ class SeqReader {
     while (stream != nullptr) {
       std::string id, seq;
       std::getline(*stream, id);
-      std::getline(*stream, seq);
-      trim(seq);
-      if (!id.empty() and !seq.empty()) {
+      if (!id.empty()) {
         std::stringstream ss;
-        ss << seq;
-        size_t cnt = 0;
         while (stream->peek() != EOF && stream->peek() != '>' && stream->peek() != '+') {
           std::getline(*stream, seq);
           trim(seq);
-          if (seq.empty())
-            break;
-          ss << seq;
-          cnt += 1;
+          if (!seq.empty())
+            ss << seq;
         }
-        next = {ss.str(), std::move(trim(id.substr(1, id.find(" "))))};
+        next = {ss.str(), std::move(trim(id.substr(1, id.size() - 1)))};
         choose_next_pos(0);
         cur_start = 0;
         if (fastq) {
