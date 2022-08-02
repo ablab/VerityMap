c93d2eb44ce94a6fdbf6a9419c2d758259c89a89
seryrzu <seryrzu@gmail.com>
2022-08-02 13:51:21 -0700

    forgot to change datatype in py wrapper - fixed

--- veritymap/main.py
+++ veritymap/main.py
@@ -17,10 +17,12 @@ from veritymap.py_src.mapper import do
 @click.option('--reads', 'reads_fname', type=click.Path(exists=True), help='File with ONT/PacBio reads')
 @click.option('-o', 'out_dir', type=click.Path(), required=True, help='Output folder')
 @click.option('-t', 'threads', type=click.INT, help='Threads', default=4)
-@click.option('-d', 'datatype', type=click.Choice(['hifi', 'ont']), help='Sequencing platform, supported types are: '
-                                                                         '"hifi" for PacBio HiFi reads and "ont" for ONT reads.'
-                                                                         'Please note that "ont" mode is experimental and '
-                                                                         'should be used with extra care')
+@click.option('-d', 'datatype',
+              type=click.Choice(['hifi-haploid', 'hifi-haploid-complete', 'hifi-diploid', 'ont-haploid-complete']),
+              help='Sequencing platform, supported types are: '
+                   '"hifi" for PacBio HiFi reads and "ont" for ONT reads.'
+                   'Please note that "ont" mode is experimental and '
+                   'should be used with extra care')
 @click.option('-f', '--no-reuse', 'no_reuse', is_flag=True, help='Do not reuse old files')
 @click.option('--careful', 'is_careful', is_flag=True, help='Run mapper in a careful mode to better detect inconsistencies. Can be time- and memory-consuming. Not recommended to run on the whole genome. ')
 @click.option('-l', 'labels', help='Comma separated list of assembly labels')
